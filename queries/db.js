8faa071d600006879fa89ce67715832b7baacee4
JBallin <JBallin@users.noreply.github.com>
2018-10-12 15:03:43 -0700

    Fix process.env.NODE_ENV

--- queries/db.js
+++ queries/db.js
@@ -1,6 +1,6 @@
 const knex = require('knex');
 
-const environment = process.NODE_ENV || 'development';
+const environment = process.env.NODE_ENV || 'development';
 const knexFile = require('../knexfile');
 
 module.exports = knex(knexFile[environment]);
