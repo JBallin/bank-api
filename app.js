6e3689cf4ea6a07d3b708aefdca07a2d35de5ce4
JBallin <JBallin@users.noreply.github.com>
2018-10-18 16:57:48 -0700

    Add logging and body parsing

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const logger = require('morgan');
 
 const app = express();
+app.use(express.json(), express.urlencoded({ extended: true }));
+if (!process.env.NODE_ENV !== 'test') app.use(logger('dev'));
 
 const port = process.env.PORT || 3000;
 
