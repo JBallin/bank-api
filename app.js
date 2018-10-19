09976cf00c8eb5b79f66f818a9255737d748d35d
JBallin <JBallin@users.noreply.github.com>
2018-10-19 15:50:51 -0700

    Clean up process.env in app.js

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const logger = require('morgan');
 
 const app = express();
 app.use(express.json(), express.urlencoded({ extended: true }));
-if (!process.env.NODE_ENV !== 'test') app.use(logger('dev'));
+const { NODE_ENV, PORT } = process.env;
+if (NODE_ENV !== 'test') app.use(logger('dev'));
 
-const port = process.env.PORT || 3000;
+const port = PORT || 3000;
 
 const indexRouter = require('./src/routes/index');
 const accountsRouter = require('./src/routes/accounts');
@@ -19,10 +20,8 @@ app.use((err, req, res) => {
   res.status(err.status).json(err);
 });
 
-if (process.env.NODE_ENV !== 'test') {
-  app.listen(port, () => {
-    console.log(`The bank is open on ${port}!`); // eslint-disable-line no-console
-  });
-}
+app.listen(port, () => {
+  if (NODE_ENV !== 'test') console.log(`The bank is open on ${port}!`); // eslint-disable-line no-console
+});
 
 module.exports = app;
