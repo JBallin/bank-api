657e4d26c9867bf4bd8463bb4a3ed0d8393f1d4d
JBallin <JBallin@users.noreply.github.com>
2018-10-20 10:04:54 -0700

    Test POST /accounts/:id/transactions

--- test/02_transactions.test.js
+++ test/02_transactions.test.js
@@ -2,6 +2,13 @@ const request = require('supertest');
 const { assert } = require('chai');
 // const knex = require('../db/queries/db');
 const app = require('../app');
+const { seeds } = require('../db/seeds/accounts');
+
+const newTransaction = {
+  title: 'Best Buy',
+  amount: 20.12,
+  pending: true,
+};
 
 describe('transactions', () => {
   describe('/accounts/:id/transactions', () => {
@@ -30,6 +37,30 @@ describe('transactions', () => {
       });
     });
 
+    describe('POST', () => {
+      it('(200)', (done) => {
+        request(app)
+          .post('/accounts/1/transactions')
+          .send(newTransaction)
+          .expect(200)
+          .expect('Content-Type', /json/)
+          .end((err, res) => {
+            if (err) done(err);
+            assert.equal(res.body, newTransaction);
+            done();
+          });
+      });
+
+      it('DNE (400)', (done) => {
+        request(app)
+          .post('/accounts/0/transactions')
+          .expect(400)
+          .end((err, res) => {
+            if (err) done(err);
+            assert.equal(res.text, 'No account with id 0');
+            done();
+          });
+      });
     });
   });
 });
