8d69fbd10b0ca7f6970754905e109b6b3e9ea29c
JBallin <JBallin@users.noreply.github.com>
2018-10-20 11:25:32 -0700

    Cascade on delete

--- db/migrations/20181020100815_transactions.js
+++ db/migrations/20181020100815_transactions.js
@@ -1,6 +1,6 @@
 exports.up = knex => knex.schema.createTable('transactions', (table) => {
   table.increments();
-  table.integer('account_id').references('accounts.id');
+  table.integer('account_id').references('accounts.id').onDelete('CASCADE');
   table.string('title').notNullable();
   table.decimal('amount').notNullable();
   table.boolean('pending').defaultTo(true);
