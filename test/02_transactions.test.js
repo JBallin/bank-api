ab61f2df781fa1c7e9bd74ce318d41a042c552b0
JBallin <JBallin@users.noreply.github.com>
2018-10-20 09:59:24 -0700

    Organize test by method

--- test/02_transactions.test.js
+++ test/02_transactions.test.js
@@ -5,27 +5,31 @@ const app = require('../app');
 
 describe('transactions', () => {
   describe('/accounts/:id/transactions', () => {
-    it('GET (200)', (done) => {
-      request(app)
-        .get('/accounts/1/transactions')
-        .expect(200)
-        .expect('Content-Type', /json/)
-        .end((err, res) => {
-          if (err) done(err);
-          assert.deepEqual(res, []);
-          done();
-        });
+    describe('GET', () => {
+      it('(200)', (done) => {
+        request(app)
+          .get('/accounts/1/transactions')
+          .expect(200)
+          .expect('Content-Type', /json/)
+          .end((err, res) => {
+            if (err) done(err);
+            assert.deepEqual(res, seeds[0].transactions);
+            done();
+          });
+      });
+
+      it('DNE (400)', (done) => {
+        request(app)
+          .get('/accounts/0/transactions')
+          .expect(400)
+          .end((err, res) => {
+            if (err) done(err);
+            assert.equal(res.text, 'No account with id 0');
+            done();
+          });
+      });
     });
 
-    it('GET (400)', (done) => {
-      request(app)
-        .get('/accounts/0/transactions')
-        .expect(400)
-        .end((err, res) => {
-          if (err) done(err);
-          assert.equal(res.text, 'No account with id 0');
-          done();
-        });
     });
   });
 });
