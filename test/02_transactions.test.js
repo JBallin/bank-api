54fc8decfad24a3b337612c2413ac61a0f40794e
JBallin <JBallin@users.noreply.github.com>
2018-10-20 12:08:04 -0700

    Use /transactions and TDD all endpoints

--- test/02_transactions.test.js
+++ test/02_transactions.test.js
@@ -1,8 +1,9 @@
 const request = require('supertest');
 const { assert } = require('chai');
-// const knex = require('../db/queries/db');
+const knex = require('../db/queries/db');
 const app = require('../app');
-const { seeds } = require('../db/seeds/00_accounts');
+const { seeds } = require('../db/seeds/01_transactions');
+const { assertMatch } = require('./utils');
 
 const newTransaction = {
   account_id: 4,
@@ -12,55 +13,110 @@ const newTransaction = {
 };
 
 describe('transactions', () => {
-  describe('/accounts/:id/transactions', () => {
+  describe('/transactions', () => {
     describe('GET', () => {
       it('(200)', (done) => {
         request(app)
-          .get('/accounts/1/transactions')
+          .get('/transactions')
           .expect(200)
           .expect('Content-Type', /json/)
           .end((err, res) => {
             if (err) done(err);
-            assert.deepEqual(res, seeds[0].transactions);
-            done();
-          });
-      });
-
-      it('DNE (400)', (done) => {
-        request(app)
-          .get('/accounts/0/transactions')
-          .expect(400)
-          .end((err, res) => {
-            if (err) done(err);
-            assert.equal(res.text, 'No account with id 0');
+            seeds.forEach((seed, i) => {
+              assertMatch(res.body[i], seed);
+            });
             done();
           });
       });
     });
 
     describe('POST', () => {
+      it('(201)', (done) => {
+        request(app)
+          .post('/transactions')
+          .send(newTransaction)
+          .expect(201)
+          .expect('Content-Type', /json/)
+          .end((err, res) => {
+            if (err) done(err);
+            assertMatch(res.body, newTransaction);
+            done();
+          });
+      });
+    });
+  });
+
+  describe('/transactions/:id', () => {
+    describe('GET', () => {
       it('(200)', (done) => {
         request(app)
-          .post('/accounts/1/transactions')
-          .send(newTransaction)
+          .get('/transactions/7')
           .expect(200)
           .expect('Content-Type', /json/)
           .end((err, res) => {
             if (err) done(err);
-            assert.equal(res.body, newTransaction);
+            assertMatch(res.body, newTransaction);
             done();
           });
       });
 
       it('DNE (400)', (done) => {
         request(app)
-          .post('/accounts/0/transactions')
+          .get('/transactions/0')
           .expect(400)
           .end((err, res) => {
             if (err) done(err);
-            assert.equal(res.text, 'No account with id 0');
+            assert.equal(res.text, 'No transaction with id 0');
             done();
           });
+      });
+    });
+
+    describe('PUT', () => {
+      it('(200)', (done) => {
+        request(app)
+          .put('/transactions/7')
+          .send({ pending: false })
+          .expect(200)
+          .expect('Content-Type', /json/)
+          .end((err, res) => {
+            if (err) done(err);
+            assertMatch(res.body, { ...newTransaction, pending: false });
+            done();
+          });
+      });
+
+      it('DNE (400)', (done) => {
+        request(app)
+          .put('/transactions/0')
+          .send({ pending: false })
+          .expect(400)
+          .end((err, res) => {
+            if (err) done(err);
+            assert.equal(res.text, 'No transaction with id 0');
+            done();
+          });
+      });
+    });
+
+    describe('DELETE', () => {
+      it('(204)', (done) => {
+        request(app)
+          .delete('/transactions/7')
+          .expect(204)
+          .end((err) => {
+            if (err) done(err);
+            knex('transactions')
+              .where('id', 7)
+              .then(res => assert.lengthOf(res, 0))
+              .then(done);
+          });
+      });
+
+      it('DNE (400)', (done) => {
+        request(app)
+          .delete('/transactions/0')
+          .expect(400, done);
       });
     });
   });
