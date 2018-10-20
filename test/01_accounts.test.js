7a8204702247f2f317829d3c1801993ad1e7fcf5
JBallin <JBallin@users.noreply.github.com>
2018-10-19 22:03:44 -0700

    Refactor to use mocked req + res and validator func

--- test/01_accounts.test.js
+++ test/01_accounts.test.js
@@ -1,7 +1,34 @@
 const request = require('supertest');
-const { assert } = require('chai');
+const { assert, expect } = require('chai');
 const knex = require('../db/queries/db');
 const app = require('../app');
+
+/* eslint-disable camelcase */
+const name = 'Michael Jordan';
+const bank_name = 'Bank of America';
+const description = 'Savings';
+const transactions = [];
+const properPayload = {
+  name,
+  bank_name,
+  description,
+  transactions,
+};
+const payloadMissingTransactions = { name, bank_name, description };
+const seed1 = {
+  id: 1,
+  name: 'John Doe',
+  bank_name: 'Bank of America',
+  description: 'Main',
+  transactions: [],
+};
+/* eslint-enable camelcase */
+
+const assertMatch = (payload, resBody) => {
+  Object.keys(payload).forEach((prop) => {
+    expect(resBody).to.have.deep.property(prop, payload[prop]); // eslint-disable-line
+  });
+};
 
 describe('/accounts', () => {
   before(() => knex.migrate.rollback()
@@ -15,8 +42,7 @@ describe('/accounts', () => {
       .expect('Content-Type', /json/)
       .end((err, res) => {
         if (err) done(err);
-        assert.isArray(res.body);
-        assert.isObject(res.body[0]);
+        assertMatch(seed1, res.body[0]);
         done();
       });
   });
